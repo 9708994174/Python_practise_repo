771cc8e39c116910338f29a889da9e7934a400bc
LAPTOP-NHFK6AVK\HP <kumar799024@gmail.com>
2022-10-18 21:33:21 +0530

    arrays

---
+++ python project2/arrays.py
@@ -0,0 +1,7 @@
+Sentence="A quick brown fox jumps on lazy dog"
+
+if "fox" in Sentence:
+    print("yes! it is there")
+else:
+    print("no")
+
---
+++ python project2/arrayspr1.py
@@ -0,0 +1,9 @@
+Sentence="The quick brown fox "
+print(Sentence[2:7])
+sentence="India is my country"
+print(sentence[2:])
+
+sentence="The quick brown fox "
+print(sentence[-2:-7])
+
+
