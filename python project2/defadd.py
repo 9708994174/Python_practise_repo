fcd8594406fa45fbd4612eb2057eb01f20e58f94
LAPTOP-NHFK6AVK\HP <kumar799024@gmail.com>
2022-11-14 21:29:16 +0530

    listpractise

--- python project2/defadd.py
+++
@@ -1,5 +0,0 @@
-def add(num1,num2):
-    sum = num1 +num2
-    print(sum)
-add(2,5)
-
---
+++ python project2/listforgit.py
@@ -0,0 +1,7 @@
+#write a programe to turn every item of list into its square
+numbers = [1,2,3,4,5]
+newlist=[]
+for i in numbers:
+    k=i**2
+    newlist.append(k)
+print(newlist)
