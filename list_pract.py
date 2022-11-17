03cb8e6f08d249e01075ad31b66b190490d5fb73
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2022-11-17 22:11:04 +0530

    list_practise

---
+++ list_pract.py
@@ -0,0 +1,39 @@
+colors=['red','blue','red','blue',22]
+cars=['Tata','Nano','alto','jeep']
+players=('Dhoni','Messi','Virat')
+numbers=[1,4,6,10]
+numbers.sort(reverse=True)
+print(numbers)
+
+# newlist=[x for x in cars if x!="Tata"]
+# print(newlist)
+# newlist=[x.upper() for x in cars]
+# print(newlist)
+
+# colors[0:1]="Yellow","pink"
+# print(colors[1])      
+# colors.insert(2,'Indigo')     -to add something where you want by indexing       
+# colors.append('BMW')          - to add something in end of list
+# colors.extend(players)        -to add two list
+# colors.remove('red')          -to remove something from list
+# colors.pop(1)                 -to remove something from list
+#del colors[0]                  -to delete
+#colors.clear()                 -to clear
+#print(colors)
+# for x in cars:
+#     print(x)
+# for x in range(len(cars)):
+#     print(cars[x])
+# x = 0
+# while x < len(cars):
+#     print(cars[x])
+#     x+=1
+
+# [print(x)for x in cars]
+# newlist=[]
+# for i in cars:
+    
+#         if 'a' in i:
+#             newlist.append(i)
+# print(newlist)
+
