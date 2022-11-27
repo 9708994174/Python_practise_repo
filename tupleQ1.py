bb60e70632e5ea31a8ebe4ca4c2140b630bb4352
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2022-11-27 20:22:17 +0530

    tupleQ1

---
+++ tupleQ1.py
@@ -0,0 +1,10 @@
+# tuples store mutiple items in a single variable
+# non homogenous
+# ordered   - fixed index value
+# unchangable / immutable
+# allows duplicate value
+
+myTuple=(1,2,3,4,4)
+print(myTuple)
+x=len(myTuple)
+print(x)
