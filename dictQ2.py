116ce530868cd0a91923ab40c012d83a8112985a
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2022-12-05 22:44:16 +0530

    dictQ2

---
+++ dictQ2.py
@@ -0,0 +1,10 @@
+Mydic = {
+    "name": "Rahul",
+    "age": 19,
+    "class": 12,
+    "percentage": 99.9
+    }
+a=Mydic.keys()
+b=Mydic.values()
+c = Mydic.items() #.......... when you print items you will get the list of tuple
+print(c)
