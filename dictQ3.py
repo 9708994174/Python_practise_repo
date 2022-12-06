9c4a5ca9dd96a4a01744d4da15f25b002438487e
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2022-12-06 22:00:48 +0530

    DictQ3

---
+++ dictQ3.py
@@ -0,0 +1,15 @@
+
+# print 95
+
+myDictionary = {
+    "class":{
+        "Student":{
+            "name": "abc",
+            "marks":{
+                "python": 90,
+                "web": 95
+            }
+        }
+    }
+}
+print(myDictionary["class"]["Student"]["marks"]["web"])
