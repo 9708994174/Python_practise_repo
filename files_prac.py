eb69d2151ef8e71471dedfa433e6486e94399f6f
Rahul kumar <114799965+9708994174@users.noreply.github.com>
2022-12-25 23:08:38 +0530

    files_practise

---
+++ files_prac.py
@@ -0,0 +1,16 @@
+# f=open("demo.txt","r")
+# print(f.read())
+# print(f.readline())
+# print(f.readline())
+
+
+# f1=open("demo1.txt","w")
+# f1.write("This is a new file")
+
+
+# f2=open("myfile.txt","w")
+# f2.write("My name is rahul \n")
+# f2.write("I am a computer Science Student\n")
+
+# for i in f:
+#    f2.write(i)
